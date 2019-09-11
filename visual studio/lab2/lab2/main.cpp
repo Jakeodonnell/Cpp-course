befe6e14ca3f282aee2125821c68a3a014872ccf
Jake <imjakeeyy@gmail.com>
2019-09-11 21:59:46 +0200

    lab2 update

--- visual studio/lab2/lab2/main.cpp
+++ visual studio/lab2/lab2/main.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	const int N = 20;
-	int counter1 = 0;
-	int counter2 = 0;
+	const int N = 10;
+	int Nmove = N;
 	int arrayOfNumbers[N];
+	int counter = 0;
 	std::cout << "prime numbers: " << std::endl;
 
-	for (int i = 0; i < N + 1; i++) {
-		arrayOfNumbers[i] = counter1;
-		counter1++;
-		std::cout << arrayOfNumbers[i] << std::endl;
+	//add N numbers from 2.
+	for (int i = 2; i < N + 2; i++) {
+		arrayOfNumbers[counter] = i;
+		std::cout << "|" << arrayOfNumbers[counter] << "|";
+		counter++;
 	}
+	std::cout <<  std::endl;
 
-	for (int i = 2; i < 100; i++)
-		for (int j = 2; j * j <= i; j++)
-		{
-			if (i % j == 0)
-				break;
-			else if (j + 1 > sqrt(i)) {
-				std::cout << i << " ";
-
+	//remove even nmumbers.
+	for (int i = 0; i < N; i++) {
+		if ((arrayOfNumbers[i] % 2 == 0) && (arrayOfNumbers[i] > 2)) {
+			Nmove--;
+			for (int j = i; j < N; j++) {
+				arrayOfNumbers[j] = arrayOfNumbers[j + 1];
 			}
-
 		}
-		
-
 	}
-	for (int i = 0; i < N + 1; i++) {
+
+
+	//Check modded array with only odd numbers.
+	for (int i = 0; i < Nmove; i++) {
+		std::cout << "|" << arrayOfNumbers[i] << "|";
+	}
+	std::cout << std::endl;
+
+	for (int i = arrayOfNumbers[0]; i < N + 2; i++) {
+		for (int j = arrayOfNumbers[0]; j < sqrt(i); j++) {
+			if ((i % j == 0) && (i > 3)) {
+				
+				break;
+			}else if((j + 1) > sqrt(i)){
+				std::cout << "add to array " << i << std::endl;
+			}
+		}
+	}
+	for (int i = 0; i < Nmove; i++) {
 		std::cout << arrayOfNumbers[i] << std::endl;
 	}
 }
+	
 	/*for (int i = 2; i < N; i++) {
 		for (int j = 2; j * j <= i; j++) {
 			std::cout << "i :" << i<< " j :" << j << std::endl;
