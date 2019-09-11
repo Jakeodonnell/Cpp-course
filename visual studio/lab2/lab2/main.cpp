89da8792ee4746641296b06e9dfc7876c7059caa
Jake <imjakeeyy@gmail.com>
2019-09-11 10:34:34 +0200

    prime fail

--- visual studio/lab2/lab2/main.cpp
+++ visual studio/lab2/lab2/main.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	const int N = 50;
+	const int N = 20;
+	int counter1 = 0;
+	int counter2 = 0;
 	int arrayOfNumbers[N];
-	int counter = 0;
 	std::cout << "prime numbers: " << std::endl;
 
-	for (int i = 2; i < N; i++) {
-		for (int j = 2; j * j <= i; j++) {
-			if (i % j == 0) {
-			}
+	for (int i = 0; i < N + 1; i++) {
+		arrayOfNumbers[i] = counter1;
+		counter1++;
+		std::cout << arrayOfNumbers[i] << std::endl;
+	}
+
+	for (int i = 2; i < 100; i++)
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+				break;
 			else if (j + 1 > sqrt(i)) {
-				arrayOfNumbers[counter] = i;
-				std::cout << arrayOfNumbers[counter] << std::endl;
+				std::cout << i << " ";
+
 			}
+
 		}
 		
-		counter++;
+
+	}
+	for (int i = 0; i < N + 1; i++) {
+		std::cout << arrayOfNumbers[i] << std::endl;
 	}
 }
-	
+	/*for (int i = 2; i < N; i++) {
+		for (int j = 2; j * j <= i; j++) {
+			std::cout << "i :" << i<< " j :" << j << std::endl;
+
+			if (i % j == 0) {
+				std::cout << "i remove " << i << std::endl;
+			}
+			else if (j + 1 > sqrt(i)) {
+				std::cout << "i add to array " << i << std::endl;
+			}
+		}
+	}*/
