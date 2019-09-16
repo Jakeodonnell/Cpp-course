5227fce9b249caa8f5614279633ecc6ac7334b19
Jake <imjakeeyy@gmail.com>
2019-09-16 19:33:40 +0200

    klar

--- lab1/hello/hello.cpp
+++ lab1/hello/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char** argv)
 {	
 	
-	cout << "Hello Wolrd! " << "nice to see you ";
+	cout << "Hello Wolrd! nice to see you, ";
 
 	for(int i = 1; i < argc; i++){
 		if((i + 1) == argc){
