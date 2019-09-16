ceffdb78b04edc7b1ade14ae456eefac46aa161a
Jake <imjakeeyy@gmail.com>
2019-09-16 18:23:51 +0200

    lab2 klar

--- visual studio/lab2/lab2/main.cpp
+++ visual studio/lab2/lab2/main.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
+#include "lab2.h"
+#include <string>
 
 int main(int argc, char** argv) {
-	//(N + 1)
-	const int N = 10;
-	int Nmove = N;
-	int arrayOfNumbers[N];
-	int counter = 0;
-	std::cout << "prime numbers: " << std::endl;
+	Lab2* l2 = new Lab2();
+	int val;
+	std::string String1 = "hej pa dej";
+	const std::string before1 = "ej";
+	const std::string after1 = "ig";
 
-	//add N numbers from 2.
-	for (int i = 2; i < N + 2; i++) {
-		arrayOfNumbers[counter] = i;
-		std::cout << "|" << arrayOfNumbers[counter] << "|";
-		counter++;
-	}
-	std::cout <<  std::endl;
+	std::string String2 = "hejjj";
+	const std::string before2 = "ej";
+	const std::string after2 = "eje";
 
-	//remove even nmumbers.
-	for (int i = 0; i < N; i++) {
-		if (((arrayOfNumbers[i] % 2) == 0) && (arrayOfNumbers[i] > 2)) {
-			for (int j = i; j < Nmove; j++) {
-				arrayOfNumbers[j] = arrayOfNumbers[j + 1];
-			}
-			Nmove--;
+
+	const char* String3 = "hej pa dejjj";
+	const char before3 = 'e';
+	const char* after3 = "aaaa";
+	char* result;
+
+
+	std::cout << "Press 1 for Assignment 1\n" << "Press 2 for Assignment 2\n" << "Press 3 for Assignment 3\n" << std::endl;
+	while (std::cin >> val) {
+		switch (val)
+		{
+			
+		case 1: val = 1;
+			l2->Assignment1();
+			std::cout << std::endl;
+			std::cout << std::endl;
+			std::cout << "Press 1 for Assignment 1\n" << "Press 2 for Assignment 2\n" << "Press 3 for Assignment 3\n" << std::endl;
+			break;
+
+		case 2: val = 2;
+			std::cout << "String: " << String1 << "\nBefore: " << before1 << "\nAfter: " << after1 << std::endl;
+			l2->Assignment2(String1, before1, after1);
+			std::cout << String1 << "\n \n \n";
+
+			std::cout << "String: " << String2 << "\nBefore: " << before2 << "\nAfter: " << after2 << std::endl;
+			l2->Assignment2(String2, before2, after2);
+			std::cout << String2 << "\n \n \n";
+			std::cout << "Press 1 for Assignment 1\n" << "Press 2 for Assignment 2\n" << "Press 3 for Assignment 3\n" << std::endl;
+			break;
+
+		case 3: val = 3;
+
+			result = l2->Assignment3(String3, before3, after3);
+			std::cout << "RESULT: " << result << "\n\n";
+			delete[] result;
+			break;
+		default:
+			std::cout << "Press 1 for Assignment 1\n" << "Press 2 for Assignment 2\n" << "Press 3 for Assignment 3\n" << std::endl;
+			break;
 		}
 	}
-
-
-	for (int i = 0; i < Nmove; i++) {
-		std::cout << "|" << arrayOfNumbers[i] << "|";
-	}
-	std::cout << std::endl;
-
-
-	//Check modded array with only odd numbers.
-	for (int i = 0; i < Nmove; i++) {
-		for (int j = i; j < Nmove; j++) {
-			//What number in the array is a multiple of arrayOfNumbers[i], remove them.
-			if (arrayOfNumbers[j] != arrayOfNumbers[i] && arrayOfNumbers[j] % arrayOfNumbers[i] == 0) {
-				for (int k = j; k < Nmove; k++) {
-					arrayOfNumbers[k] = arrayOfNumbers[k + 1];
-				}
-				Nmove--;
-			}
-		}
-	}
-
-	//Final array of numbers(should be primes only)
-	std::cout << "primes: ";
-	for (int i = 0; i < Nmove; i++) {
-		std::cout << "|" << arrayOfNumbers[i] << "|";
-	}
-
 }
 	
 	
